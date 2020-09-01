fad7ef14f08f8f76d2360c23230b4af3f9cafd60
ririkurashu <70560477+ririkurashu@users.noreply.github.com>
2020-09-01 07:36:38 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js'); 
+const bot = new Discord.Client();
+const comms = require("./comms.js"); // Подключаем файл с командами для бота
+const fs = require('fs'); // Подключаем родной модуль файловой системы node.js  
+let config = require('./botconfig.json'); //подключаем файл конфигурации
+let token = config.token; //"достаём" токен
+let prefix = config.prefix; //"достаём" префикс
+
+//создаём ссылку-приглашение для бота
+/*
+bot.on('ready', () => { 
+    console.log(`Запустился бот ${bot.user.username}`);
+    bot.generateInvite(["ADMINISTRATOR"]).then(link => { 
+        console.log(link);
+    });
+});
+*/
+
+bot.on("ready", function(){ /* Бот при запуске должен отправить в терминал сообщение «[Имя бота] запустился!» */
+	console.log(bot.user.username + " запустился!");
+	console.log("https://discord.com/api/oauth2/authorize?client_id=749320971890196600&permissions=3148800&scope=bot");
+});
+
+
+bot.on("message", msg => { // Реагирование на сообщения
+	if(msg.author.username != bot.user.username && msg.author.discriminator != bot.user.discriminator){
+    	var comm = msg.content.trim()+" ";
+	    var ok = false;
+	    var comm_name = comm.slice(0, comm.indexOf(" "));
+	    var messArr = comm.split(" ");
+	    for(comm_count in comms.comms){
+	    	var comm2 = prefix + comms.comms[comm_count].name;
+	    	if(comm2 == comm_name){
+	    		comms.comms[comm_count].out(bot, msg, messArr);
+	    	}
+	    }
+    } 
+});
+
+
+bot.on("voiceStateUpdate", async (oldState, newState) => {
+	if(newState.member.user.username != bot.user.username && newState.member.user.discriminator != bot.user.discriminator && newState.channel != oldState.channel && newState.channel)
+	{
+		console.log("1337");
+		var voiceChannel = newState.member.voice.channel;
+		voiceChannel.join().then(connection =>{
+		const dispatcher = connection.play('./cumzone.mp3');
+		dispatcher.on("speaking", speaking => {
+			if(!speaking) voiceChannel.leave();
+		});
+	}).catch(err => console.log(err));
+	}
+})
+
+bot.login(token);
