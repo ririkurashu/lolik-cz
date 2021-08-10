e0f3b54269a6e31deca2b6a3111e8b80a88ecdd7
ririkurashu <70560477+ririkurashu@users.noreply.github.com>
2021-08-11 01:53:12 +0300

    Add files via upload

--- comms.js
+++ comms.js
@@ -6,57 +6,58 @@ const prefix = config.prefix;
 
 async function stop (bot, mess, args) 
 {
-        if(mess.author.id != "643129279298928641") return mess.channel.send("сосеш")
+        if(mess.author.id != "643129279298928641") return mess.channel.send("You don't have permission to stop me!")
         else{
-                await mess.channel.send("Бот выключается.");
+                await mess.channel.send("The bot is turning off...");
                 process.exit();
         }
 }
 
 function play (bot, mess, args) {
-        if(mess.member.voice.channel)
-        {
-                if(mess.member.voice.channel.permissionsFor(mess.member.client.user).has("CONNECT") && mess.member.voice.channel.permissionsFor(mess.member.client.user).has("SPEAK")){
-                        var voiceChannel = mess.member.voice.channel;
-                        if(args[1])
-                        {
-                                var files = fs.readdirSync("./");
-                                for(i = 0; i < files.length; i++){
-                                        if(files[i].indexOf(".mp3") == -1 || files[i].indexOf(".mp3.sfk") > -1) {
-                                                files.splice(i, 1);
-                                                i--;
+        if (message.channel.type === "GUILD_TEXT") {
+                if(mess.member.voice.channel) {
+                        if(mess.member.voice.channel.permissionsFor(mess.member.client.user).has("CONNECT") && mess.member.voice.channel.permissionsFor(mess.member.client.user).has("SPEAK")){
+                                var voiceChannel = mess.member.voice.channel;
+                                if(args[1]) {
+                                        var files = fs.readdirSync("./");
+                                        for(i = 0; i < files.length; i++){
+                                                if(files[i].indexOf(".mp3") == -1 || files[i].indexOf(".mp3.sfk") > -1) {
+                                                        files.splice(i, 1);
+                                                        i--;
+                                                }
+                                                else files[i] = files[i].slice(0, files[i].lastIndexOf(".mp3"));
                                         }
-                                        else files[i] = files[i].slice(0, files[i].lastIndexOf(".mp3"));
+                                        if(files.indexOf(args[1].toLowerCase()) > -1){
+                                                voiceChannel.join().then(connection => {
+                                                        const dispatcher = connection.play(`./${args[1].toLowerCase()}.mp3`);
+                                                        dispatcher.on('finish', () => voiceChannel.leave());
+                                                }).catch(err => console.log(err));
+                                        }
+                                        else mess.channel.send("Couldn't find a file with that name.\nIf not sure, please type `cz!tracklist` and check the list.");
                                 }
-                                if(files.indexOf(args[1].toLowerCase()) > -1){
+                                else {
                                         voiceChannel.join().then(connection => {
-                                                const dispatcher = connection.play(`./${args[1].toLowerCase()}.mp3`);
+                                                const dispatcher = connection.play('./cumzone.mp3');
                                                 dispatcher.on('finish', () => voiceChannel.leave());
                                         }).catch(err => console.log(err));
                                 }
-                                else mess.channel.send("Не могу найти файл с таким названием.\nВызовите `cz!tracklist` и сверьтесь со списком.");
-                        }
-                        else{
-                                voiceChannel.join().then(connection => {
-                                        const dispatcher = connection.play('./cumzone.mp3');
-                                        dispatcher.on('finish', () => voiceChannel.leave());
-                                }).catch(err => console.log(err));
                         }
                 }
+                else mess.channel.send("For that command to work, you need to be in a voice chat.");
         }
-        else mess.channel.send("Чтобы эта команда работала, вы должны находиться в войсчате.");
 }
 
 function playme (bot, mess, args) {
-        if(mess.member.voice.channel)
-        {
-                tools.greeting(mess.member);
+        if (message.channel.type === "GUILD_TEXT") {
+                if(mess.member.voice.channel) {
+                        tools.greeting(mess.member);
+                }
+                else mess.channel.send("For that command to work, you need to be in a voice chat.");
         }
-        else mess.channel.send("Чтобы эта команда работала, вы должны находиться в войсчате.");
 }
 
 function help (bot, mess, args) {
-        mess.channel.send("**Команды бота:**\n`cz!play` — приглашает вас в зону кончания.\n`cz!play <название>` — проигрывает конкретный файл приветствия.\n`cz!tracklist` — показывает список названий всех mp3 файлов.\n`cz!playme` — проигрывает ваше персональное приветствие.");
+        mess.channel.send("**Bot commands:**\n`cz!play` — welcomes you to the cum zone.\n`cz!play <trackname>` — plays a certain music file.\n`cz!tracklist` — shows the full list of mp3 track names.\n`cz!playme` — plays your personal greeting.");
 }
 
 function tracklist (bot, mess, args) {
@@ -72,7 +73,7 @@ function tracklist (bot, mess, args) {
                         mp3files += "\n`" + files[i] + "`";
                 }
         }
-        mess.channel.send("Доступные музыкальные файлы:" + mp3files);
+        mess.channel.send("Available music files:" + mp3files);
 }
 
 var comms_list = [
